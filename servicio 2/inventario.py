af605d1d436be2e4901d4d3fe564d5bc86055d24
danahiezer <dansantacruz5@gmail.com>
2026-03-12 17:55:45 -0300

    comentarios para mejor comprension del flujo de servicio inventario

--- servicio 2/inventario.py
+++ servicio 2/inventario.py
@@ -4,40 +4,40 @@ import psycopg2
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
+    level=logging.INFO,  # --> livel de log que voy a estar usando
+    format="%(asctime)s - %(levelname)s - %(message)s"  # --> el formato en el que se va a ver mi log
 )
 
-app = FastAPI()
+app = FastAPI() # --> creacion del framework con motor fastAPI
 
-token = "dan321"
+token = "dan321" # --> llave o clave secreta para mi servicio
 
 conn = psycopg2.connect(
     host="localhost",
-    database="inventarioDB",
+    database="inventarioDB",  # --> conexion con mi base de datos
     user="postgres",
     password="dan"
 )
-
+# verifica el token 
 def verificaToken(authorization: str = Header()):
     try:
-        clave = authorization.split(" ")[1]
-        jwt.decode(clave,token,algorithms=["HS256"])
+        clave = authorization.split(" ")[1] # --> divide el token con espacio y accede al segundo valor (token) con [1]
+        jwt.decode(clave,token,algorithms=["HS256"]) # --> verifica la firma y si no es correcta jwt.decode la rechaza
     except:
         logging.error("token invalido - acceso denegado")
-        raise HTTPException(status_code=401, detail="token invalido")
+        raise HTTPException(status_code=401, detail="token invalido") # --> log y estado de codigo 
 
 
 @app.get("/token")
 def generarToken():
-    Token = jwt.encode({"token": "admin"},token,algorithm="HS256")
+    Token = jwt.encode({"token": "admin"},token,algorithm="HS256") # crea el token basandose en mi clave y le da una firma o la "cifra"
     return {"token": Token}
 
 @app.get("/inventario")
 def obtenerProductos(authorization: str = Header()):
     verificaToken(authorization)
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM productos")
+    cursor.execute("SELECT * FROM productos")   # --> obtiene los productos de mi base de datos inventario
     productos = cursor.fetchall()
     logging.info("se obtuvo los datos de productos")
     return {"productos": productos}
@@ -45,7 +45,7 @@ def obtenerProductos(authorization: str = Header()):
 @app.get("/inventario/{id}")
 def obtenerProductoid(id: int,authorization: str = Header()):
     verificaToken(authorization)
-    cursor = conn.cursor()
+    cursor = conn.cursor()                      # --> obtiene productos de la bse de datos inventario pero por su id
     cursor.execute(
         "SELECT * FROM productos WHERE id = %s ",
         (id,)
@@ -54,7 +54,7 @@ def obtenerProductoid(id: int,authorization: str = Header()):
     
     if producto is None:
         logging.info(f"producto no encontrado - id: {id}")
-        raise HTTPException(status_code=402, detail="producto no encontrado")
+        raise HTTPException(status_code=402, detail="producto no encontrado") # --> log y estado de codigo de la peticion 
     logging.info(f"se obtuvo producto - id: {id}")
     return {"producto": producto}
 
@@ -68,7 +68,7 @@ def crearProductos(
     cursor = conn.cursor()
     cursor.execute(
         "INSERT INTO productos (nombre,precio,stock) VALUES (%s,%s,%s)",
-        (nombre,precio,stock)
+        (nombre,precio,stock)                                      # --> envia o inserta esos productos en mi base de datos
     )
     conn.commit()
     logging.info("se insertaron productos correctamente")
@@ -82,7 +82,7 @@ def actualizarProducto(
     stock: int,
     authorization: str = Header()):
     verificaToken(authorization)
-    cursor = conn.cursor()
+    cursor = conn.cursor()              # --> actualiza los productos basandose por su id 
     cursor.execute(
         "UPDATE productos " \
         "SET nombre = %s,precio = %s,stock = %s " \
@@ -98,7 +98,7 @@ def eliminarProducto(
     id: int,
     authorization: str = Header()):
     verificaToken(authorization)
-    cursor = conn.cursor()
+    cursor = conn.cursor()        # --> elimina ese producto de la base de datos inventario
     cursor.execute(
         "DELETE FROM productos " \
         "WHERE id = %s",
